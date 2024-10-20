e3b0af1bdc9c500414b44b1245d9055d06bdedb3
m10rten <qmaartenenzo@gmail.com>
2024-10-20 15:19:22 +0200

    Add github actions and format

--- apps/server/main.ts
+++ apps/server/main.ts
@@ -5,8 +5,9 @@ import { add, square } from "@monorepo/calculator";
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello Deno!"));
-app.get("/add/:first/:second", (c) =>
-  c.json(add(Number(c.req.param().first), Number(c.req.param().second)))
+app.get(
+  "/add/:first/:second",
+  (c) => c.json(add(Number(c.req.param().first), Number(c.req.param().second))),
 );
 app.get("/square/:value", (c) => c.json(square(Number(c.req.param().value))));
 
