df168e13e7bb335c52a55c3279a4f310aa92001c
m10rten <qmaartenenzo@gmail.com>
2024-10-20 17:39:49 +0200

    init for database

---
+++ scripts/mongo_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Waiting 10 seconds for MongoDB to start..."
+
+sleep 10
+
+echo mongo_setup.sh time now: `date +"%T" `
+mongosh --host mongodb:27017 <<EOF
+  var cfg = {
+    "_id": "rs0",
+    "version": 1,
+    "members": [
+      {
+        "_id": 0,
+        "host": "mongodb:27017",
+        "priority": 2
+      }
+    ]
+  };
+  rs.initiate(cfg);
+
+  db.adminCommand({ "setDefaultRWConcern" : 1 })
+EOF
